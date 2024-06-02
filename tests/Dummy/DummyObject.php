00aa68645310dd5d39c7f3173d7662857d10a005
Ehyiah <rei_eva@hotmail.com>
2024-06-02 15:12:31 +0200

    add requirements and dummy classes

---
+++ tests/Dummy/DummyObject.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Ehyiah\ApiDocBundle\Tests\Dummy;
+
+use DateTime;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
+
+class DummyObject
+{
+    public string $stringNotNullable;
+    public bool $booleanNotNullable;
+    public ?bool $booleanNullable = null;
+    public ?string $stringNullable = null;
+    public ?DateTime $datetimeNullable = null;
+    public DateTime $datetimeNotNullable;
+    public DummyEnum $enumNotNullable;
+    public ?DummyEnum $enumNullable = null;
+    public DummyObject2 $objectNotNullable;
+    public ?DummyObject2 $objectNullable = null;
+    /**
+     * @var Collection<DummyObject2>
+     */
+    public Collection $collectionOfDummyObject2;
+
+    public function __construct()
+    {
+        $this->collectionOfDummyObject2 = new ArrayCollection();
+    }
+}
